3c8ea3d284a0541efcc8539e6c87c9fd02e379cc
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-04-12 14:29:57 +0200

    feat: create federated identity credentials (#5)
    * feat: create federated identity credentials
    Fixes #3
    
    * chore: update example

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ resource "azurerm_user_assigned_identity" "this" {
 
   tags = var.tags
 }
+
+resource "azurerm_federated_identity_credential" "this" {
+  for_each = var.federated_identity_credentials
+
+  name                = each.value["name"]
+  resource_group_name = var.resource_group_name
+  parent_id           = azurerm_user_assigned_identity.this.id
+  audience            = each.value["audiences"]
+  issuer              = each.value["issuer"]
+  subject             = each.value["subject"]
+}
--- variables.tf
+++ variables.tf
@@ -13,6 +13,19 @@ variable "location" {
   type        = string
 }
 
+variable "federated_identity_credentials" {
+  description = "A map of federated identity credentials to create for this Managed identity."
+
+  type = map(object({
+    name      = string
+    audiences = optional(list(string), ["api://AzureADTokenExchange"])
+    issuer    = string
+    subject   = string
+  }))
+
+  default = {}
+}
+
 variable "tags" {
   description = "A map of tags to assign to the resources."
   type        = map(string)
