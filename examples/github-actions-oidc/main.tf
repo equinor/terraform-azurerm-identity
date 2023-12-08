522158d7d194fbc2598fe301994d47a791b03cb7
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-08 10:32:01 +0100

    docs: development and testing (#11)

--- examples/github-actions-oidc/main.tf
+++ examples/github-actions-oidc/main.tf
@@ -6,18 +6,13 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "rg-${random_id.example.hex}"
-  location = var.location
-}
-
 module "identity" {
   # source = "github.com/equinor/terraform-azurerm-identity?ref=v0.0.0"
   source = "../.."
 
   identity_name       = "id-${random_id.example.hex}"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
 
   federated_identity_credentials = {
     # Allow authentication from environment "development"
